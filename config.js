d04ae50e391ed2f328865d815c23092823b6180d
nguyentran2410 <96384204+nguyentran2410@users.noreply.github.com>
2021-12-19 23:29:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    introDesc: `T có điều muốn nói`,
     btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    title: 'xin lỗi vì đã quạo với m',
+    desc: 'm không trả lời hoặc thoát ra, tức là m ko giận t ',
+    btnYes: 'hết giận r😝',
+    btnNo: 'đang rất giận😡 ',
+    question:'thật sự hết giận r đúng ko',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    reply: 'hết giận r',
+    mess: 't biết mà😆',
+    messDesc: '🙆‍♀️🙆‍♀️🙆‍♀️',
+    btnAccept: '🙆‍♀️🙆‍♀️🙆‍♀️',
+    messLink: 'https://www.facebook.com/quy.quyen.7583'
 }
